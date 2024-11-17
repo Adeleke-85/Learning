6c592f7a45510f989f09b961041d46275b96c371
Adeleke-85 <adelekemuinat17@gmail.com>
2024-11-17 16:40:37 +0100

    add age calculator

---
+++ practice/script.js
@@ -0,0 +1,5 @@
+// let a = prompt("what is your name")
+// let b = Math.random()
+// let c = b * 100000000
+// let d = Math.round(c)
+// alert("My name is " + a + " and my networth is $" + d + " by 2025 ")
